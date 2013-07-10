7f34909b4c4ea323b36b748fdb1c60e578ebf678
James Acres <jamesacres@gmail.com>
2013-07-10 23:52:31 +0100

    read index.html and send contents as response

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var retval = '';
+    var buffer = fs.readFileSync('index.html');
+    if (!buffer) {
+        retval = 'Failed to read file';
+    } else {
+        retval = buffer.toString('utf-8');
+    }
+    response.send(retval);
 });
 
 var port = process.env.PORT || 5000;
